9b5a38560a80f1e812dfd2e5ec224d7406fe71f4
NCarteazy <carter.nicholas.r@gmail.com>
2016-08-04 15:01:45 -0700

    showing choices

--- ctrl.js
+++ ctrl.js
@@ -5,7 +5,7 @@ angular.module('namer', [])
 	
 	$scope.name = {
 			"p1":"dog",
-			"p2string":""
+			"p2keys":""
 			};
 
 	$scope.biomes = [
@@ -83,13 +83,99 @@ angular.module('namer', [])
 		}
 	];
 
+	$scope.resourceCombinations = [
+		{
+			"keys" : 'a',
+			"abv" : 'shp',
+		},
+		{
+			"keys" : 'b',
+			"abv" : 'sut',
+		},
+		{
+			"keys" : 'c',
+			"abv" : 'mul',
+		},
+		{
+			"keys" : 'd',
+			"abv" : 'bup',
+		},
+		{
+			"keys" : 'e',
+			"abv" : 'tar',
+		},
+		{
+			"keys" : 'ab',
+			"abv" : 'shs',
+		},
+		{
+			"keys" : 'ac',
+			"abv" : 'shm',
+		},
+		{
+			"keys" : 'ad',
+			"abv" : 'sht',
+		},
+		{
+			"keys" : 'ae',
+			"abv" : 'shb',
+		},
+		{
+			"keys" : 'bc',
+			"abv" : 'sum',
+		},
+		{
+			"keys" : 'bd',
+			"abv" : 'str',
+		},
+		{
+			"keys" : 'be',
+			"abv" : 'sub',
+		},
+		{
+			"keys" : 'cd',
+			"abv" : 'mbl',
+		},
+		{
+			"keys" : 'ce',
+			"abv" : 'mtr',
+		},
+		{
+			"keys" : 'de',
+			"abv" : 'btr',
+		},
+		{
+			"keys" : 'abce',
+			"abv" : 'nth',
+		},
+		{
+			"keys" : 'abcde',
+			"abv" : 'bth',
+		}
+	];
 
 
 	$scope.resourceChange = function(){
-		$scope.p2string = "dog"
+		$scope.p2keys = ""
+		$scope.p2names = ""
 		for (var resource in $scope.resources) {
-			$scope.p2string = $scope.p2string + $scope.resources[resource].key
+			if($scope.resources[resource].boo) {
+				$scope.p2keys = $scope.p2keys + $scope.resources[resource].key
+				$scope.p2names = $scope.p2names + "/" + $scope.resources[resource].name
+			};
 		};
-
+		for (var resourceCombination in $scope.resourceCombinations) {
+			if($scope.resourceCombinations[resourceCombination].keys === $scope.p2keys) {
+				$scope.p2 = $scope.resourceCombinations[resourceCombination].abv
+			}
+			else if ($scope.resourceCombinations[resourceCombination].keys.length >= 3) {
+				if($scope.resourceCombinations[resourceCombination].keys.indexOf('d') != -1) {
+					$scope.p2 = 'nth'
+				};
+				else {
+					$scope.p2 = 'bth'
+				};
+			};
+		};
 	}
 }]);
